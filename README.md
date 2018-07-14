37de1dc593bfed54c6bb6b7aad05cda99e53220b
Nickolay Demin <demin.n.m@yandex.ru>
2018-07-14 18:57:03 +0700

    add: --allow-root

--- README.md
+++ README.md
@@ -4,4 +4,4 @@
 * Matplotlib
 
 ## Launch example
-docker run -d --name "jupyter" -p 8888:8888 -v /home/files/project/ipython_nb:/home/ipython_nb -e "PASSWORD=pass" demin/jupyter-notebook-python
+docker run -d --name "jupyter" -p 8888:8888 -v /mnt/data/development/ipython_nb:/home/ipython_nb --restart unless-stopped -e "PASSWORD=pass" demin/jupyter-notebook-python
--- start_jupyter.sh
+++ start_jupyter.sh
@@ -3,4 +3,4 @@
 HASH=$(python3 -c "from IPython.lib import passwd; print(passwd('${PASSWORD}'))")
 unset PASSWORD
 
-jupyter notebook --ip=0.0.0.0 --port=8888 --no-browser --notebook-dir=/home/ipython_nb --NotebookApp.password="$HASH"
+jupyter notebook --ip=0.0.0.0 --port=8888 --allow-root --no-browser --notebook-dir=/home/ipython_nb --NotebookApp.password="$HASH"
